aff94da1df095da8dc2e62f4c8370eede9758707
Michael Ion <michael.ion.mi@outlook.com>
2019-09-05 18:02:31 -0400

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,21 @@
 
+const btn = document.querySelector("#set-color");
+const colorInput = document.querySelector("#color-field");
+const myBrush = document.querySelector(".brush");
+
+
+
+function setColor(){
+    btn.addEventListener('click', (ev)=>{
+    let color = getColor();
+        myBrush.style.background = color;
+      ev.preventDefault();
+    })
+
+}
+
+function getColor(){
+    return colorInput.value;
+}
+
+setColor();
